888dfeb96113c10bd7a104061d7d10be1a493586
bformless <56992637+bformless@users.noreply.github.com>
2021-11-14 04:21:17 +0100

    some minor changes

--- Main.cpp
+++ Main.cpp
@@ -1,46 +1,74 @@
+// Author: Michael Raebiger
+// aka: bformless
+// (c) 2019
+// Date: 07-31-2019
+// rewritten on: 11-14-2021
+// Todo: Code cleanup
+
 #define VC_EXTRALEAN
 #define WIN32_LEAN_AND_MEAN
 #include <iostream>
-#include <time.h>
+#include <chrono>
 #include <cmath>
 #include <vector>
 #include <thread>
 typedef unsigned long long ULL;
 using namespace std;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration;
+using std::chrono::milliseconds;
 
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
 void Info() {
-	ios_base::sync_with_stdio(false);
-	cout << "=================================================\n" \
-		"||     SIEVE OF ERATOSTHENES (x64_Windows)     ||\n" \
-		"||    Multithreaded Prime Numbers Algorithm    ||\n" \
-		"=================================================\n" \
-		"||     written by Michael Raebiger (c)2019     ||\n" \
-		"=================================================\n" \
-		"=================================================\n" \
-		"||        Warning! Use at your own risk!       ||\n" \
-		"||                                             ||\n" \
-		"||     1 billion numbers to search require     ||\n" \
-		"||       1 Gigabyte of free memory (RAM)!      ||\n" \
-		"=================================================\n" << endl;
+	    cout << "=================================================\n" \
+		    "||                                             ||\n" \
+		    "||           'SIEVE OF ERATOSTHENES'           ||\n" \
+		    "||                                             ||\n" \
+		    "||                (x64_Windows)                ||\n" \
+		    "||                                             ||\n" \
+		    "||       Very fast (but not the fastest)       ||\n" \
+		    "||                                             ||\n" \
+		    "||   'Multithreaded Prime Numbers Algorithm'   ||\n" \
+		    "||                                             ||\n" \
+		    "=================================================\n" \
+		    "|| written in C++  by Michael Raebiger (c)2019 ||\n" \
+		    "=================================================\n" \
+		    "                                                 \n" \
+		    "*************************************************\n" \
+		    "**                                             **\n" \
+		    "**  ***  Warning!  Use at your own risk!  ***  **\n" \
+		    "**                                             **\n" \
+		    "**                                             **\n" \
+		    "**   >+ 64 billion numbers to search require   **\n" \
+		    "**    >+ 64 Gibibyte of free memory (RAM)!     **\n" \
+		    "**                                             **\n" \
+		    "**       Example: 1 billion = 1 Gibibyte       **\n" \
+		    "**                                             **\n" \
+		    "**                                             **\n" \
+		    "**  ***  Warning!  Use at your own risk!  ***  **\n" \
+		    "**                                             **\n" \
+		    "*************************************************\n" \
+		    "*************************************************\n" << endl;
 }
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
-
 auto UserInput() -> ULL {
-	ios_base::sync_with_stdio(false);
-	static ULL reader;
-	static bool inPut = true;
+	ULL reader;
+	bool inPut = true;
 	do {
 		cout << "How many numbers do you want to check for Primes?\n";
-		cout << "(2 to 254.999.999.998)\n\n" \
+		cout << "(2 to 18.446.744.069.414.584.318)\n\n" \
 			"-> \a";
 
 		if (cin >> reader) {
 
-			// input worked // possible max. 18.446.744.073.709.551.614
-			if (reader < 2 || reader > 2549999999998Ui64) {
+			// input worked
+			// possible max. 18.446.744.073.709.551.616 = 2^64 bit
+			// restricted to 2^64 bit minus 2^32 bit + 2
+			// To handle such a big number you need at least 16 Exbibyte of RAM
+
+			if (reader < 2 || reader > 18446744069414584318Ui64) {
 				cin.clear();
 				cin.ignore(numeric_limits<streamsize>::max(), '\n');
 				inPut = true;
@@ -61,21 +89,18 @@ auto UserInput() -> ULL {
 }
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
-
 auto askThreads(const ULL &numbers) -> ULL {
 
-	ios_base::sync_with_stdio(false);
-
-	static ULL numberOfThreads = 1;
-	static bool inPut = true;
+	ULL numberOfThreads = 1;
+	bool inPut = true;
 	do {
 		unsigned int reader;
-		static ULL numThreadsMax = thread::hardware_concurrency();
+		ULL numThreadsMax = thread::hardware_concurrency();
 		if (numThreadsMax == 0) {
 			numThreadsMax = 1;
 		}
 
-		if (numbers < 500000)
+		if (numbers < 3000000)
 		{
 			numThreadsMax = (numThreadsMax / numThreadsMax);
 		}
@@ -109,30 +134,27 @@ auto askThreads(const ULL &numbers) -> ULL {
 }
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
+static void DisplayElapsedTime(const double& elapsedTime) {
 
-void DisplayElapsedTime(const double& elapsedTime) {
-	ios_base::sync_with_stdio(false);
-
-	cout << "\nIt took " << (elapsedTime / double(CLOCKS_PER_SEC)) << " seconds for the Sieve of Eratosthenes Algorithm" << endl;
+	cout << "\nIt took " << (elapsedTime) << " seconds for the optimized Sieve of Eratosthenes Algorithm." << endl;
 }
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
+auto PrimeCounter(const ULL &sieveSize, bool *primeSieveArray) -> ULL {
 
-auto __forceinline PrimeCounter(const ULL &UPPER_BOUND, bool *primesList) -> ULL {
-	ios_base::sync_with_stdio(false);
-	static ULL counter = 0;
+	ULL counter = 0;
 
-	if (UPPER_BOUND < 2) {}
+	if (sieveSize < 2) {}
 
-	else if (UPPER_BOUND < 3) {
+	else if (sieveSize < 3) {
 		counter = 1;
 	}
 	else {
 		counter = 1;
 
 		#pragma omp parallel for
-		for (ULL i = 3; i <= UPPER_BOUND; i += 2) {
-			if (!primesList[i]) {
+		for (ULL i = 3; i <= sieveSize; i += 2) {
+			if (!primeSieveArray[i]) {
 				counter++;
 			}
 		}
@@ -141,23 +163,19 @@ auto __forceinline PrimeCounter(const ULL &UPPER_BOUND, bool *primesList) -> ULL
 }
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
+static void DisplayNumberOfPrimes(ULL &count) {
 
-void DisplayNumberOfPrimes(ULL &count) {
-	ios_base::sync_with_stdio(false);
-
-	static ULL primesCount = count;
-	cout << "\nPrime numbers found: " << primesCount << endl;
+	ULL primesCount = count;
+	cout << "found: " << primesCount << endl;
 }
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
+static void LastPrime(ULL &sieveSize, bool *primeSieveArray) {
 
-void __forceinline LastPrime(ULL &arraySize, bool *primes) {
-
-	ios_base::sync_with_stdio(false);
-	cout << "\nLast Prime number: ";
+	cout << "\nThe last Prime number is ";
 
 	#pragma omp parallel for
-	for (ULL i = (arraySize - 1); i >= 2; i -= 2) {
+	for (ULL i = (sieveSize - 1); i >= 2; i -= 2) {
 
 		if ((i - 1) < 2 && i >= 0) {
 			cout << "2" << endl;
@@ -166,7 +184,7 @@ void __forceinline LastPrime(ULL &arraySize, bool *primes) {
 		if (i % 2 == 0) {
 			--i;
 		}
-		if (!primes[i]) {
+		if (!primeSieveArray[i]) {
 			cout << i << endl;
 			break;
 		}
@@ -174,30 +192,29 @@ void __forceinline LastPrime(ULL &arraySize, bool *primes) {
 }
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
+static void PrintIt(ULL& sieveSize, bool *primeSieveArray) {
 
-void __forceinline PrintIt(ULL& UPPER_BOUND, bool *primes) {
-	ios_base::sync_with_stdio(false);
-	static ULL counter = 1;
-	static char reader;
-	static bool inPut = true;
+	ULL counter = 1;
+	char reader;
+	bool inPut = true;
 
 	do {
-		cout << "\nDo you want to print all Prime numbers? (y/n)\n" \
+		cout << "\nDo you want to print all Prime numbers to the console? (y/n)\n" \
 			"-> \a";
 		if (cin >> reader) {
 			// input worked
 			switch (reader) {
 			case 'y':
-				if (UPPER_BOUND < 3) {
+				if (sieveSize < 3) {
 					cout << "\n2\n" << endl;
 				}
 				else {
 					cout << "\n\t2";
 
 					#pragma omp parallel for
-					for (ULL i = 3; i <= UPPER_BOUND; i += 2)
+					for (ULL i = 3; i <= sieveSize; i += 2)
 					{
-						if (!primes[i]) {
+						if (!primeSieveArray[i]) {
 							cout << "\t" << i;
 							counter++;
 							if (counter % 10 == 0) {
@@ -227,28 +244,26 @@ void __forceinline PrintIt(ULL& UPPER_BOUND, bool *primes) {
 		}
 	} while (inPut);
 }
-
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
-void wait() {
-	ios_base::sync_with_stdio(false);
+static void wait() {
 
 	cout << "\n\nPress <ENTER> to end program.\a" << endl;
 	cin.clear();
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	cin.get();
 }
-
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
-static void __forceinline Sieve(ULL x,ULL y,const ULL &UPPER_BOUND,const ULL &sqrtUpperBound, bool* primes) {
+/* static void Sieve(ULL x, ULL y, const ULL& sieveSize, const ULL& sqrtSieveSize, bool* primeSieveArray) {
 
-	#pragma omp parallel for
-	for (ULL i = x; i <= (sqrtUpperBound); i += y) {
-		#pragma omp parallel for
-		for (ULL test = i; test <= sqrtUpperBound; test += y)
+
+	for (ULL i = x; i <= (sqrtSieveSize); i += y) {
+		
+
+		for (ULL test = i; test <= sqrtSieveSize; test += y)
 		{
-			if (primes[(test * test)]) {
+			if (primeSieveArray[(test)]) {
 				i += y;
 			}
 			else {
@@ -256,37 +271,36 @@ static void __forceinline Sieve(ULL x,ULL y,const ULL &UPPER_BOUND,const ULL &sq
 			}
 		}
 
-		#pragma omp parallel for
-		for (ULL j = i; (i * j) <= UPPER_BOUND; j += 2) {
-			primes[(i * j)] = 1;
+		for (ULL j = i; (i * j) <= sieveSize; j += 2) {
+			primeSieveArray[(i * j)] = 1;
 		}
 	}
 }
+*/
 //----------------------------------------------------------------------------
-
-static void __forceinline Sieve2(ULL x, ULL y, const ULL &UPPER_BOUND, const ULL &sqrtUpperBound, bool *primes)
+static void Sieve2(ULL x, ULL y, const ULL &sieveSize, const ULL &sqrtSieveSize, bool *primeSieveArray)
 {
+	const ULL step = y;
 
-	static const ULL step = y;
-
-	while (x <= sqrtUpperBound)
+	while (x <= sqrtSieveSize)
 	{
-		
-                #pragma omp parallel for
-		for (ULL test = x; test <= sqrtUpperBound; test += step)
+		for (ULL test = x; test < sqrtSieveSize; test += step)
 		{
-			if (primes[(test * test)]) {
-				x += step;
-			}
+
+			if (primeSieveArray[(test)]) {
+					x += step;
+				}
+				
 			else {
 				break;
 			}
 		}
+
 		y = x;
 
-		while ((x * y) <= UPPER_BOUND)
+		while ((x * y) <= sieveSize)
 		{
-			primes[(x * y)] = 1;
+			primeSieveArray[(x * y)] = 1;
 			y += 2;
 		}
 		x += step;
@@ -294,57 +308,80 @@ static void __forceinline Sieve2(ULL x, ULL y, const ULL &UPPER_BOUND, const ULL
 }
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
+static void FindPrimesWithThreads(const ULL &numThreads, const ULL &sieveSize, const ULL &sqrtSieveSize, bool* primesSieveArray) {
+	
+	vector<thread> threadVect;
+	ULL startNum = 1;
 
-void __forceinline FindPrimesWithThreads(const ULL &numThreads, const ULL &userInput, const ULL &sqrtUserInput, bool* primes) {
-	
-	ios_base::sync_with_stdio(false);
-	
-	static vector<thread> threadVect;
-	static ULL startNum = 1;
 	#pragma omp parallel for
-	for (ULL x = 0; x < numThreads; x++) {
-		threadVect.emplace_back(Sieve2, (startNum + 2), (numThreads * 2), userInput, sqrtUserInput, primes);
+	for (ULL x = 0; x < numThreads; ++x) {
+
+		threadVect.emplace_back(Sieve2, (startNum + 2), (numThreads * 2), sieveSize, sqrtSieveSize, primesSieveArray);
 		startNum += 2;
 	}
-	#pragma omp parallel for
+    #pragma omp parallel for
 	for (auto& t : threadVect) {
 		t.join();
 	}
 }
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	Info();
-	double start, end, elapsedTime;
 
-	ULL userInput = UserInput();
-	ULL arraySize = (userInput + 1);
-	ULL sqrtUserInput = (ULL)sqrt(userInput);
+	double elapsedTime;
+	ULL sieveSize = UserInput();
+	ULL primeSieveArraySize = (sieveSize);
+	ULL sqrtSieveSize = (ULL)sqrt(sieveSize);
+	ULL numOfThreads = 0;
+	ULL count = 0;
 
 	cout << "\nBuilding array... ";
-	__declspec (align (64)) static bool *primes = new bool[arraySize] { false };
+	__declspec (align (64)) static bool *primeSieveArray = new bool[primeSieveArraySize] { false };
 	cout << "Ready.\n" << endl;
 
-	ULL numOfThreads = askThreads(userInput);
+	numOfThreads = askThreads(sieveSize);
 
-	start = clock();
-	FindPrimesWithThreads(numOfThreads, userInput, sqrtUserInput, primes);
-	end = clock();
+	if (numOfThreads < 2) {
+		auto start = high_resolution_clock::now();
+		Sieve2(3, 2, sieveSize, sqrtSieveSize, primeSieveArray);
+		auto end = high_resolution_clock::now();
+		duration<double, std::milli> ms_double = end - start;
+		elapsedTime = (ms_double.count() / 1000);
+	}
+	else {
+		auto start = high_resolution_clock::now();
+		FindPrimesWithThreads(numOfThreads, sieveSize, sqrtSieveSize, primeSieveArray);
+		auto end = high_resolution_clock::now();
+		duration<double, std::milli> ms_double = end - start;
+		elapsedTime = (ms_double.count() / 1000);
+	}
 
-	ULL count = PrimeCounter(userInput, primes);
+	DisplayElapsedTime(elapsedTime);
+
+	cout << "\nCounting Primes... ";
+
+	count = PrimeCounter(sieveSize, primeSieveArray);
 
 	DisplayNumberOfPrimes(count);
 
-	LastPrime(arraySize, primes);
+	LastPrime(primeSieveArraySize, primeSieveArray);
 
-	elapsedTime = (end - start);
-	DisplayElapsedTime(elapsedTime);
+	PrintIt(sieveSize, primeSieveArray);
 
-	PrintIt(userInput, primes);
+	cout << "\nDeleting the array and cleaning up... ";
 
-	delete[] primes;
+	delete[] primeSieveArray;
+	elapsedTime = 0;
+	sieveSize = 0;
+	primeSieveArraySize = 0;
+	sqrtSieveSize = 0;
+	numOfThreads = 0;
+	count = 0;
+
+	cout << "Ready.";
+
 	wait();
 }
 //----------------------------------------------------------------------------
